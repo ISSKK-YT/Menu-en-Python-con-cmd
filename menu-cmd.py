5dd4bedf592684db0f31325cd4da689ef64a213f
ISSKK-YT <163071578+ISSKK-YT@users.noreply.github.com>
2024-03-12 15:34:26 -0500

    Add files via upload

---
+++ menu-cmd.py
@@ -0,0 +1,10 @@
+print("--------------Menu en Python---------------------")
+print(" 1. Hello World(Hola Mundo)")
+Opcion = input("Opcion: ")
+
+#Crea una if o utiliza elif para crear mas opciones
+if Opcion == "1":
+    print("Hello World")
+    print("Hola Mundo")
+else:
+    print("Esta Opcion no existe o no es una opcion")
